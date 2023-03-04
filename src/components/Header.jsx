9915664befa0bf3590a59fbcea00e826307830f6
Arif99 <arifrahamancob@gmail.com>
2023-03-04 17:41:34 +0530

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import { AppBar,Button, Toolbar, Typography } from '@mui/material'
+import { AppBar,Button, Toolbar, Typography,useTheme } from '@mui/material'
 import { IconButton } from '@mui/material';
 import { Badge } from '@mui/material';
 import React from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { Box } from '@mui/system';
 import { useSelector } from 'react-redux';
 import { getItemCount } from '../utils';
@@ -25,14 +26,18 @@ const Search =styled("section")(({theme})=>({
 }));
 function SearchBar(){
     const products=useSelector((state)=>state.products.value)
+    const theme=useTheme()
     return <Search>
         <Autocomplete
         disablePortal
         id="combo-box-demo"
         options={Array.from(products,(prod)=>({id:prod.id,label:prod.title}))}
-        sx={{ width: 300 }}
+        sx={{ width: "100%" ,[theme.breakpoints.up('md')]: {
+           display:"flex",
+          },}}
         renderInput={(params) => <TextField {...params} />}
         />
+        <IconButton size="large" aria-label="Show cart Items Count" color="inherit"><SearchOutlinedIcon sx={{}}/></IconButton>
     </Search>
 }
 
@@ -41,17 +46,19 @@ export default function Header() {
     const count=getItemCount(cartItmes)
   return (
     <AppBar position="sticky">
-        <Toolbar>
+        <Toolbar sx={{display:"flex", justifyContent:"space-between"}}>
+            <Box>
             <Typography variant='h6' color="inherit" sx={{flexGrow:1}}>COBSHOP</Typography>
-            <Box sx={{display:{md:"flex"}}}>
-                <SearchBar/>
+            </Box>
+            <Box sx={{display:"flex",alignItems:"center" , width:"500px"}}>
+                <SearchBar sx={{display:{md:"flex"}}}/>
                 <IconButton size="large" aria-label="Show cart Items Count" color="inherit">
                     <Badge badgeContent={count} color="error">
                         <ShoppingCartIcon/>
                     </Badge>
                 </IconButton>
+                <Button color="inherit">LogIn</Button>
             </Box>
-            <Button color="inherit">LogIn</Button>
         </Toolbar>
     </AppBar>
   )
